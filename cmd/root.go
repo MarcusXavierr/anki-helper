9284f85f9aaad6707c89dd84a1dd10d870d739e8
Marcus Xavier <marcus.xavier@appmax.com.br>
2022-08-11 22:17:00 -0300

    Add description

--- cmd/root.go
+++ cmd/root.go
@@ -12,14 +12,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "anki-helper",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use: "anki-helper",
+	Long: `anki helper is a CLI focused on helping you to save the new words you learn in a language.
+And then you can save those words in your anki`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },

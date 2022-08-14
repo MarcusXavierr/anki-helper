0a1b5ceab06640a412a6fb42b9905a6fec9ba090
Marcus Xavier <marcus.xavier@aluno.ufop.edu.br>
2022-08-14 14:59:01 -0300

    Create normalize helper

--- app/dictionary/dictionary_helper.go
+++ app/dictionary/dictionary_helper.go
@@ -7,6 +7,18 @@ func CleanMeaningResults(meaning Meaning) Meaning {
 	return Meaning{PartOfSpeech: meaning.PartOfSpeech, Definitions: definitions}
 }
 
+func (d DictionaryApiResponse) Normalize() DictionaryApiResponse {
+	meanings := []Meaning{}
+	for _, meaning := range d.Meanings {
+		meaning = CleanMeaningResults(meaning)
+		if len(meaning.Definitions) > 0 {
+			meanings = append(meanings, meaning)
+		}
+	}
+
+	return DictionaryApiResponse{Word: d.Word, Meanings: meanings}
+}
+
 func removeEmptyDefinitions(meaning Meaning, definitions []Definition) []Definition {
 	for _, definition := range meaning.Definitions {
 		if definition.Def != "" && definition.Example != "" {

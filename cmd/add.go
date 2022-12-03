3bf391e59b7170bfece0e94e1f81afa18ce6f0e9
Marcus <marcus.xavier@aluno.ufop.edu.br>
2022-12-02 23:34:38 -0300

    Add viper and flags to deal with user's paths

--- cmd/add.go
+++ cmd/add.go
@@ -5,6 +5,7 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/MarcusXavierr/anki-helper/app/IO"
@@ -12,20 +13,35 @@ import (
 	"github.com/MarcusXavierr/anki-helper/app/sentenceCheck"
 	"github.com/MarcusXavierr/anki-helper/app/utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
-const defaultConfigFilename = "anki-config"
+const defaultConfigFilename = ".anki-config"
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add [sentence]",
-	Short: "Adds a new word or sentence you learned",
-	Run: func(cmd *cobra.Command, args []string) {
-		saveSentenceAndPrintDefinition(cmd, args)
-	},
+func NewAddCmd() *cobra.Command {
+	write := utils.UserFilePath{WriteFile: "", TrashFile: ""}
+
+	addCmd := &cobra.Command{
+		Use:   "add [sentence]",
+		Short: "Adds a new word or sentence you learned",
+		Run: func(cmd *cobra.Command, args []string) {
+			saveSentenceAndPrintDefinition(cmd, args, write)
+		},
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return initializeConfig(cmd)
+		},
+	}
+
+	defaultNewWordsPath := fmt.Sprintf("%s/english_words/words.txt", IO.GetHomeDir())
+	defaultTrashPath := fmt.Sprintf("%s/english_words/trash.txt", IO.GetHomeDir())
+
+	addCmd.Flags().StringVarP(&write.WriteFile, "new-words-file-path", "n", defaultNewWordsPath, "só um teste")
+	addCmd.Flags().StringVarP(&write.TrashFile, "trash-file-path", "t", defaultTrashPath, "só um teste trash")
+	return addCmd
 }
 
-func saveSentenceAndPrintDefinition(cmd *cobra.Command, args []string) {
+func saveSentenceAndPrintDefinition(cmd *cobra.Command, args []string, userFiles utils.UserFilePath) {
 	definition, _ := cmd.Flags().GetBool("definition")
 	sentence := getSentenceFromArgs(args)
 
@@ -33,7 +49,7 @@ func saveSentenceAndPrintDefinition(cmd *cobra.Command, args []string) {
 		printDefinition(sentence)
 	}
 
-	saveSentence(sentence)
+	saveSentence(sentence, userFiles)
 }
 
 func printDefinition(sentence string) {
@@ -47,22 +63,22 @@ func printDefinition(sentence string) {
 	}
 }
 
-func saveSentence(sentence string) {
-	wordsTrackerFile, trash := getFiles()
+func saveSentence(sentence string, userFiles utils.UserFilePath) {
+	wordsTrackerFile, trash := getFiles(userFiles)
 
 	if !sentenceCheck.CheckIfSentenceExists(os.Stdout, sentence, wordsTrackerFile, trash) {
 		utils.WriteSentenceOnFile(sentence, wordsTrackerFile.FilePath)
 	}
 }
 
-func getFiles() (IO.File, IO.File) {
-	wordsTrackerFile := IO.File{FilePath: IO.GetHomeDir() + "/english_words/words.txt"}
-	trash := IO.File{FilePath: IO.GetHomeDir() + "/english_words/trash.txt"}
+func getFiles(userFiles utils.UserFilePath) (IO.File, IO.File) {
+	wordsTrackerFile := IO.File{FilePath: userFiles.WriteFile}
+	trash := IO.File{FilePath: userFiles.TrashFile}
 	return wordsTrackerFile, trash
 }
 
 func init() {
-	rootCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(NewAddCmd())
 	rootCmd.PersistentFlags().BoolP("definition", "d", false, "")
 }
 
@@ -73,4 +89,33 @@ func getSentenceFromArgs(args []string) string {
 	}
 	sentence := args[0]
 	return sentence
+}
+
+func initializeConfig(cmd *cobra.Command) error {
+	v := viper.New()
+
+	v.SetConfigName(defaultConfigFilename)
+
+	v.AddConfigPath(IO.GetHomeDir())
+
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	bindFlags(cmd, v)
+	return nil
+}
+
+func bindFlags(cmd *cobra.Command, v *viper.Viper) {
+	cmd.Flags().VisitAll(func(flag *pflag.Flag) {
+		configName := flag.Name
+
+		if !flag.Changed && v.IsSet(configName) {
+			val := v.Get(configName)
+			cmd.Flags().Set(flag.Name, fmt.Sprintf("%v", val))
+		}
+	})
+
 }

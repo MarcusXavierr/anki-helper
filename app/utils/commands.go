09b8377270f8a24f30b43dd345ccc55c74de7571
Marcus <marcus.xavier@aluno.ufop.edu.br>
2023-02-16 00:21:44 -0300

    feat(cmd): changing used api on command

--- app/utils/commands.go
+++ app/utils/commands.go
@@ -10,6 +10,8 @@ import (
 	"github.com/MarcusXavierr/anki-helper/app/IO"
 	"github.com/MarcusXavierr/anki-helper/app/check"
 	"github.com/MarcusXavierr/anki-helper/app/dictionary"
+	"github.com/MarcusXavierr/wiktionary-scraper/pkg/scraper"
+	"github.com/samber/lo"
 )
 
 type UserFilePath struct {
@@ -33,6 +35,18 @@ func PrettyPrintDefinition(response dictionary.DictionaryApiResponse) {
 
 }
 
+func printGreen(text string) {
+	IO.PrintGreen(os.Stdout, text)
+}
+
+func printCyan(text string) {
+	IO.PrintCyan(os.Stdout, text)
+}
+
+func printPink(text string) {
+	IO.PrintPink(os.Stdout, text)
+}
+
 func Usage() {
 	var message string = fmt.Sprintf("usage: %s \"sentence to add\"\n", os.Args[0])
 	IO.PrintRed(os.Stdout, message)
@@ -45,4 +59,26 @@ func WriteSentenceOnFile(sentence, filePath string) {
 	check.Check(err)
 	message := fmt.Sprintf("Sentence %q added successfully\n", sentence)
 	IO.PrintGreen(os.Stdout, message)
+}
+
+func PrintWiktionary(response scraper.Response, sentence string) {
+	printGreen("Result for expression: ")
+	IO.PrintWithColor(os.Stdout, fmt.Sprintf("%s\n", sentence), string("\033[38;2;243;134;48m"))
+	lo.ForEach(response.Usages, printUsage)
+}
+
+func printUsage(usage scraper.Usage, _ int) {
+	printGreen("\n--------------------------------------------------------\n")
+	printPink("Part of speech: " + usage.PartOfSpeech + "\n")
+	lo.ForEach(usage.Definitions, printDefinition)
+	fmt.Println("")
+
+}
+
+func printDefinition(definition scraper.Definition, _ int) {
+	printGreen("Definition: " + definition.WordDefinition + "\n")
+	lo.ForEach(definition.Examples, func(example scraper.Example, _ int) {
+		printCyan("Example: " + string(example) + "\n")
+	})
+	printGreen("\n")
 }
--- cmd/add.go
+++ cmd/add.go
@@ -15,6 +15,8 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+
+	"github.com/MarcusXavierr/wiktionary-scraper/pkg/api"
 )
 
 const defaultConfigFilename = ".anki-config"
@@ -46,12 +48,25 @@ func saveSentenceAndPrintDefinition(cmd *cobra.Command, args []string, userFiles
 	sentence := getSentenceFromArgs(args)
 
 	if definition {
-		printDefinition(sentence)
+		// printDefinition(sentence)
+		printWiktionaryDefinition(sentence)
+
 	}
 
 	saveSentence(sentence, userFiles)
 }
 
+func printWiktionaryDefinition(sentence string) {
+	resp, err := api.GetDefinition("https://en.wiktionary.org/api/rest_v1/page/definition/", sentence)
+
+	if err == api.NotFoundError {
+		IO.PrintRed(os.Stdout, "word not found on dictionary api\n\n")
+	}
+
+	if err == nil {
+		utils.PrintWiktionary(resp, sentence)
+	}
+}
 func printDefinition(sentence string) {
 	url := "https://api.dictionaryapi.dev/api/v2/entries/en/" + sentence
 	response, err := dictionary.GetDefinition(url)

c8470937da4d8e641f67ebaed93661008395af84
Marcus <marcus.xavier@aluno.ufop.edu.br>
2023-02-22 00:28:09 -0300

    fix: add missing pkg

---
+++ internal/tui/colors.go
@@ -0,0 +1,12 @@
+package tui
+
+import "github.com/charmbracelet/lipgloss"
+
+var Purple = lipgloss.Color("#9F2EEB")
+var UltraViolet = lipgloss.Color("#6A4C93")
+var BrightPink = lipgloss.Color("#EA526F")
+var ColumbiaBlue = lipgloss.Color("#B4D4EE")
+var Mantis = lipgloss.Color("#5FAD56")
+
+var White = lipgloss.Color("#E7ECEF")
+var Black = lipgloss.Color("#000000")

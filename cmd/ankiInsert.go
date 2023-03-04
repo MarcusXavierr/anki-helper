b443dcdb63409bf4c930d7a1840ea9ef42aa19a2
Marcus <marcus.xavier@aluno.ufop.edu.br>
2023-03-04 18:34:01 -0300

    feat(cmd): integrating tui code with cli command

--- cmd/ankiInsert.go
+++ cmd/ankiInsert.go
@@ -26,31 +26,64 @@ to quickly create a Cobra application.`,
 		Run: func(cmd *cobra.Command, args []string) {
 			dir, path := filepath.Split(write.WriteFile)
 			x := os.DirFS(dir)
-			result, err := IO.GetWords(x, path, 1)
-
+			// result, err := IO.GetWords(x, path, 1)
+			//
+			// if err != nil {
+			// 	panic(err)
+			// }
+			//
+			// err = IO.MoveSentenceToTrash(write.TrashFile, write.WriteFile, result[0])
+			//
+			// if err != nil {
+			// 	panic(err)
+			// }
+			//
+			// fmt.Println(result)
+			//
+			// fmt.Println(config.DeckName)
+			// fmt.Println(config.ModelName)
+			sentences, err := IO.GetWords(x, path, config.MaxItems)
 			if err != nil {
-				panic(err)
+				fmt.Printf("Error reading file: %s\n", path)
+				os.Exit(1)
 			}
 
-			err = IO.MoveSentenceToTrash(write.TrashFile, write.WriteFile, result[0])
+			setLogFile()
 
-			if err != nil {
-				panic(err)
+			cards := ankiconnect.CreateCardDataMatrix(sentences, config)
+			m := ankiinsert.MakeModel(config, cards, write)
+
+			p := tea.NewProgram(m, tea.WithAltScreen(), tea.WithMouseAllMotion())
+
+			if _, err := p.Run(); err != nil {
+				fmt.Println("Error running program:", err)
+				os.Exit(1)
 			}
-
-			fmt.Println(result)
-
 		},
 	}
 
 	defaultNewWordsPath := fmt.Sprintf("%s/english_words/words.txt", IO.GetHomeDir())
 	defaultTrashPath := fmt.Sprintf("%s/english_words/trash.txt", IO.GetHomeDir())
+	defaultManualPath := filepath.Join(IO.GetHomeDir(), "english_words", "manual.txt")
 
 	ankiInsertCmd.Flags().StringVarP(&write.WriteFile, "new-words-file-path", "n", defaultNewWordsPath, "path to new words file")
 	ankiInsertCmd.Flags().StringVarP(&write.TrashFile, "trash-file-path", "t", defaultTrashPath, "path to learned words file")
+	ankiInsertCmd.Flags().StringVarP(&write.ManualInsertFile, "manual-file-path", "m", defaultManualPath, "path to the file that store sentences that could not be inserted automatically")
 
+	ankiInsertCmd.Flags().IntVarP(&config.MaxItems, "number-of-items", "i", 0, "The number of cards to be created")
 	ankiInsertCmd.Flags().StringVar(&config.DeckName, "deck-name", "", "Name of your anki deck")
 	ankiInsertCmd.Flags().StringVar(&config.ModelName, "model-name", "", "your anki model (first you need to create one)")
 
 	return ankiInsertCmd
+}
+
+func setLogFile() {
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
 }

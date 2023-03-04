4119094e709a29cb1facf69101a801371bb27e71
Marcus <marcus.xavier@aluno.ufop.edu.br>
2023-03-04 18:31:21 -0300

    feat(tui): implementing the main usage of the ankiInsert TUI

--- internal/tui/commands/anki_insert/anki_insert.go
+++ internal/tui/commands/anki_insert/anki_insert.go
@@ -1,103 +1,88 @@
 package ankiinsert
 
 import (
+	"errors"
 	"fmt"
-	"strings"
 
-	ankiconnect "github.com/MarcusXavierr/anki-helper/pkg/anki-connect"
-	"github.com/charmbracelet/bubbles/list"
+	"github.com/MarcusXavierr/anki-helper/pkg/IO"
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
-
-type cardItem struct {
-	card ankiconnect.CardData
-}
-
-func MakeCardItems(sentenceCards ankiconnect.SentenceCards) []list.Item {
-	listItems := []list.Item{}
-	for _, card := range sentenceCards.Cards {
-		listItems = append(listItems, cardItem{card: card})
-	}
-
-	return listItems
-}
-
-func (c cardItem) Title() string {
-	return fmt.Sprintf("%q", strings.TrimSpace(c.card.Example))
-}
-
-func (c cardItem) Description() string {
-	return c.card.Definition
-}
-
-func (c cardItem) FilterValue() string {
-	return c.card.Example
-}
-
-type model struct {
-	ankiConfig ankiconnect.AnkiConfig
-	list       list.Model
-	isLoading  bool
-	sentences  []ankiconnect.SentenceCards
-	error      error
-}
-
-func MakeModel(config ankiconnect.AnkiConfig, sentences []ankiconnect.SentenceCards) model {
-	item, sentences := pop(sentences)
-
-	var err error
-
-	if item.Error != nil {
-		err = item.Error
-	}
-
-	if len(item.Cards) == 0 {
-		err = fmt.Errorf("nao vai dar nao")
-	}
-
-	m := model{
-		ankiConfig: config,
-		list:       list.New(MakeCardItems(item), list.NewDefaultDelegate(), 0, 0),
-		isLoading:  false,
-		sentences:  sentences,
-		error:      err,
-	}
-	m.list.Title = item.Sentence
-	return m
-}
+var theresNoCards = errors.New("There's no Cards")
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return m.spinner.Tick
 }
 
 func (m model) View() string {
-	if m.error != nil {
-		return docStyle.Render("there was an error, press 'space' to continue")
+	// tá feio, pode firar func sla
+	if m.error == theresNoCards {
+		m.list.Title += " - Could not find examples"
+	} else if m.error == emptyListPop {
+		return "Good bye!!\nPress q or space to escape\n"
+	} else if m.error != nil {
+		return docStyle.Render("there was an error, press 'space' to continue " + m.error.Error())
+	}
+
+	if m.isLoading {
+		return fmt.Sprintf("\n\n %s loading... \n\n", m.spinner.View())
 	}
 
 	return docStyle.Render(m.list.View())
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	if len(m.sentences) == 0 && m.error == nil {
+	if len(m.sentences) == 0 && m.error != nil && m.error != theresNoCards {
 		return m, tea.Quit
 	}
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		if m.error != nil && msg.String() == tea.KeySpace.String() {
+		if msg.String() == tea.KeySpace.String() {
 			h, w := m.list.Height(), m.list.Width()
-			m := MakeModel(m.ankiConfig, m.sentences)
+			m := MakeModel(m.ankiConfig, m.sentences, m.userFiles)
 			m.list.SetSize(w, h)
+			moveSentence(m)
 			return m, nil
+		}
+
+		if msg.String() == tea.KeyCtrlC.String() {
+			return m, tea.Quit
 		}
 
 	case tea.WindowSizeMsg:
 		h, v := docStyle.GetFrameSize()
 		m.list.SetSize(msg.Width-h, msg.Height-v)
+
+	case insertMsg:
+		//Ok, como eu faço agr? Eu crio mais uma msg pra botar pra mimir (ou uso uma que tem), dps crio um comando que insere no anki, trata erro, move palavra de arquivo, e retorna uma msg falando oq deu (podendo escrever no error do model, sla). E por fim, mando um comando que finaliza o loading da lista
+		// cmd := m.list.StartSpinner()
+		return m, tea.Sequence(makeStartLoadMsg(), makeStopLoadMsg(msg.item))
+	case startLoadMsg:
+		m.isLoading = true
+		return m, spinner.Tick
+
+	case stopLoadMsg:
+		// pode virar func
+		err := msg.item.card.InsertCard("http://localhost:8765")
+		if err == nil {
+			err = IO.MoveSentenceToFile(m.userFiles.TrashFile, m.userFiles.WriteFile, msg.item.card.Sentence)
+		}
+
+		h, w := m.list.Height(), m.list.Width()
+		m = MakeModel(m.ankiConfig, m.sentences, m.userFiles)
+		m.list.SetSize(w, h)
+		if m.error == nil {
+			m.error = err
+		}
+		return m, nil
+	}
+
+	if m.isLoading {
+		return m, nil
 	}
 
 	var cmd tea.Cmd
--- internal/tui/commands/anki_insert/utils.go
+++ internal/tui/commands/anki_insert/utils.go
@@ -3,7 +3,10 @@ package ankiinsert
 import (
 	"fmt"
 
+	"github.com/MarcusXavierr/anki-helper/pkg/IO"
 	ankiconnect "github.com/MarcusXavierr/anki-helper/pkg/anki-connect"
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/lipgloss"
 )
 
 var emptyListPop = fmt.Errorf("tried to pop an empty list")
@@ -17,4 +20,19 @@ func pop(slice []ankiconnect.SentenceCards) (ankiconnect.SentenceCards, []ankico
 		}, slice
 	}
 	return slice[len(slice)-1], slice[:len(slice)-1]
+}
+
+func moveSentence(model model) error {
+	if model.error != theresNoCards {
+		return nil
+	}
+
+	return IO.MoveSentenceToFile(model.userFiles.ManualInsertFile, model.userFiles.WriteFile, model.actualSentence)
+}
+
+func makeSpinner() spinner.Model {
+	spin := spinner.New()
+	spin.Spinner = spinner.Monkey
+	spin.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	return spin
 }
